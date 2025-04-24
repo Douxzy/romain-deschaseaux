9e93f0b33f563e41db377b65391494c47c4f536f
Douxzy <romain.descha2006@gmail.com>
2025-04-24 13:29:59 +0200

    Update a propos.php

--- a-propos.php
+++ a-propos.php
@@ -10,10 +10,10 @@
                 <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">HTML5 / CSS3 / JavaScript / PHP / MySQL / Tailwind / BootStrap</span>
                 <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">Java / Python</span>
                 <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">Symfony / Laravel</span>
-                <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">WordPress / Shopify</span>
+                <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">WordPress / Shoify</span>
                 <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">Unity / Godot / Unreal Engine</span>
                 <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">Scrum / Agile </span>
-                <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">Git / Github / Gitlab / Notion / Canva / FileZilla</span>
+                <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">Git / Github / Gitlab / Notion / Canva / FileZilla / Figma    </span>
                 <span class="bg-purple-100 text-purple-700 dark:bg-purple-700 dark:text-white px-3 py-1 rounded-full text-sm">Windows 11/10 / MacOS / Linux / Android / Ios</span>
             </div>
         </div>
@@ -28,35 +28,74 @@
         </div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
             <div class="bg-white dark:bg-gray-800 shadow-md p-6 text-center">
-                <div class="font-semibold mb-2">Dév. Web & Web Mobile Fullstack - Bac+3</div>
+                <div class="font-semibold mb-2">Dév. Web & Web Mobile Fullstack - Bac+3 <br>En cours</div>
                 <div class="text-sm text-gray-500 mb-1">2024 - 2027</div>
                 <div class="text-purple-500 text-sm">Coding Factory by ESIEE-IT</div>
             </div>
             <div class="bg-white dark:bg-gray-800 shadow-md p-6 text-center">
-                <div class="font-semibold mb-2">Dév. Web & Web Mobile Fullstack - Bac+2</div>
+                <div class="font-semibold mb-2">Dév. Web & Web Mobile Fullstack - Bac+2 <br>Obtenu</div>
                 <div class="text-sm text-gray-500 mb-1">2024</div>
                 <div class="text-purple-500 text-sm">O'clock</div>
             </div>
             <div class="bg-white dark:bg-gray-800 shadow-md p-6 text-center">
-                <div class="font-semibold mb-2">Créez votre site web avec HTML5 et CSS3</div>
+                <div class="font-semibold mb-2">Créez votre site web avec HTML5 et CSS3 <br>Obtenu</div>
                 <div class="text-sm text-gray-500 mb-1">2023</div>
                 <div class="text-purple-500 text-sm">Open Classrooms</div>
             </div>
             <div class="bg-white dark:bg-gray-800 shadow-md p-6 text-center">
-                <div class="font-semibold mb-2">Concevez votre site web avec PHP et MySQL</div>
+                <div class="font-semibold mb-2">Concevez votre site web avec PHP et MySQL <br>Obtenu</div>
                 <div class="text-sm text-gray-500 mb-1">2023</div>
                 <div class="text-purple-500 text-sm">Open Classrooms</div>
             </div>
             <div class="bg-white dark:bg-gray-800 shadow-md p-6 text-center">
-                <div class="font-semibold mb-2">Apprenez à programmer avec JavaScript</div>
+                <div class="font-semibold mb-2">Apprenez à programmer avec JavaScript <br>Obtenu</div>
                 <div class="text-sm text-gray-500 mb-1">2023</div>
                 <div class="text-purple-500 text-sm">Open Classrooms</div>
             </div>
-        </div>
-        <div class="mt-8 text-center">
-            <a href="contact.php" class="text-purple-700 dark:text-purple-300 hover:underline">Envie de collaborer ? Contactez-moi →</a>
+            <div class="bg-white dark:bg-gray-800 shadow-md p-6 text-center">
+                <div class="font-semibold mb-2">Assr 1 et 2 <br>Obtenus</div>
+                <div class="text-sm text-gray-500 mb-1">2020 - 2022</div>
+                <div class="text-purple-500 text-sm">College Général</div>
+            </div>
+            <div class="bg-white dark:bg-gray-800 shadow-md p-6 text-center">
+                <div class="font-semibold mb-2">Permis B <br>En cours</div>
+                <div class="text-sm text-gray-500 mb-1">2025</div>
+                <div class="text-purple-500 text-sm">Auto-Ecole</div>
+            </div>
         </div>
     </div>
+</div>
+<div class="w-full bg-gray-100 dark:bg-gray-900 py-12">
+    <div class="max-w-6xl mx-auto px-4">
+        <div class="flex items-center justify-center mb-12">
+            <div class="flex-grow border-t border-gray-300 dark:border-gray-700"></div>
+            <span class="mx-4 text-2xl font-semibold flex items-center text-gray-900 dark:text-white">💼 Expériences Professionnelles</span>
+            <div class="flex-grow border-t border-gray-300 dark:border-gray-700"></div>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div class="bg-white dark:bg-gray-800 shadow-md p-6">
+                <div class="font-semibold mb-2">Développeur wordpress Freelance</div>
+                <div class="text-sm text-gray-500 mb-1">2024 - Aujourd'hui</div>
+                <div class="text-purple-500 text-sm mb-2">Guersan Marketing - Freelance</div>
+                <p class="text-gray-700 dark:text-gray-300 text-sm">Création de plugin Wordpress, Création d'articles sponsorisés, Utilisation de ChatGPT/Midjourney/Ideogram, Gestion de blogs Wordpres</p>
+            </div>
+            <div class="bg-white dark:bg-gray-800 shadow-md p-6">
+                <div class="font-semibold mb-2">Développeur Web Freelance</div>
+                <div class="text-sm text-gray-500 mb-1">2024 - Aujourd'hui</div>
+                <div class="text-purple-500 text-sm mb-2">Auto-entrepreneur</div>
+                <p class="text-gray-700 dark:text-gray-300 text-sm">Création de sites vitrines et e-commerce, maintenance web, intégration de solutions personnalisées (WordPress, Shopify, développement sur-mesure).</p>
+            </div>
+            <div class="bg-white dark:bg-gray-800 shadow-md p-6">
+                <div class="font-semibold mb-2">Monteur et Dépanneur PC</div>
+                <div class="text-sm text-gray-500 mb-1">2023 - 2024</div>
+                <div class="text-purple-500 text-sm mb-2">Antanak Association loi 1901 - Bénévole dans l'installation d'ordinateur Linux</div>
+                <p class="text-gray-700 dark:text-gray-300 text-sm">Reconfigurer des pc et portables sous GNU/Linux, chercher des pannes sur ban de test, Démonter pour le recyclage des composants</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="mt-8 text-center">
+    <a href="contact.php" class="text-purple-700 dark:text-purple-300 hover:underline">Envie de collaborer ? Contactez-moi →</a>
 </div>
 
 <?php require_once "footer.php"; ?>
